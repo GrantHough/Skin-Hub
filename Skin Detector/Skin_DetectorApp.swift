606cce12c373712dd5a8ec99141a9c627f2edf77
GrantHough <89223313+GrantHough@users.noreply.github.com>
2021-12-23 13:32:38 -0800

    Initial Commit

---
+++ Skin Detector/Skin_DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skin_DetectorApp.swift
+//  Skin Detector
+//
+//  Created by Grant Hough on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Skin_DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
